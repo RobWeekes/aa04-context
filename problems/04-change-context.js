ad963507aad3ba295d7b6be645933008582e136c
robweekes <weekes.robert@gmail.com>
2024-04-30 17:23:59 -0700

    problem 4

--- problems/04-change-context.js
+++ problems/04-change-context.js
@@ -1,6 +1,40 @@
 function changeContext(func, obj) {
-  // Your code here 
+    // should return the result of the function func invoked
+    // with the object obj as its context
+    return func.call(obj);
 }
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+function extractName() {
+  return this.name;
+}
+
+const kristen = new Person('Kristen');
+console.log(changeContext(extractName, kristen)); // => Kristen
+
+
+
+// more straighforward -- instance method inside class,
+// changeContext func (call) not needed
+
+// class Person {
+  //     constructor(name) {
+//       this.name = name;
+//     }
+
+//     // instance method
+//     extractName() {
+//     return this.name;
+// }
+// }
+// console.log(kristen.extractName());
+
+
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
